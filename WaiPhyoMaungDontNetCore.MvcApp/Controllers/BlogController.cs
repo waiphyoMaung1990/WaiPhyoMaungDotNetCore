710da8051af49989f3be1b7cdc4ee699f67be4ba
ko phyo <taryarphyo1990@gmail.com>
2024-02-06 20:27:57 +0630

    [Complete] Mvc Blog Read

---
+++ WaiPhyoMaungDontNetCore.MvcApp/Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WaiPhyoMaungDontNetCore.MvcApp.Models;
+using System.Collections.Generic; // Add this namespace for List<T>
+
+namespace WaiPhyoMaungDontNetCore.MvcApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly AppDbContext _appdbcontext;
+
+        public BlogController(AppDbContext context)
+        {
+            _appdbcontext = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<BlogDataModel> blogs = await _appdbcontext.Blogs.ToListAsync();
+            return View(blogs); // Pass the list of blogs to the view
+        }
+    }
+}
---
+++ WaiPhyoMaungDontNetCore.MvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WaiPhyoMaungDontNetCore.MvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
