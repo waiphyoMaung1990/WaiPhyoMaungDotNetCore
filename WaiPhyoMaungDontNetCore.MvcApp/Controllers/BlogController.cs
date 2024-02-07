20e4518183b3143b40e77b5245e476ed17a3aac9
ko phyo <taryarphyo1990@gmail.com>
2024-02-07 08:59:42 +0630

    [Complete] Mvc Create,Update,Delete

--- WaiPhyoMaungDontNetCore.MvcApp/Controllers/BlogController.cs
+++ WaiPhyoMaungDontNetCore.MvcApp/Controllers/BlogController.cs
@@ -16,8 +16,70 @@ namespace WaiPhyoMaungDontNetCore.MvcApp.Controllers
 
         public async Task<IActionResult> Index()
         {
-            List<BlogDataModel> blogs = await _appdbcontext.Blogs.ToListAsync();
+            List<BlogDataModel> blogs = await _appdbcontext.Blogs.OrderByDescending(x=>x.Blog_Id).ToListAsync();
             return View(blogs); // Pass the list of blogs to the view
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Save(BlogDataModel requestModel)
+        {
+            await _appdbcontext.Blogs.AddAsync(requestModel);
+            await _appdbcontext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var item = await _appdbcontext.Blogs.FirstOrDefaultAsync(x => x.Blog_Id == id);
+
+            if (item == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            _appdbcontext.Remove(item);
+            await _appdbcontext.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task <ActionResult> Edit(int id)
+        {
+            var item = await _appdbcontext.Blogs.FirstOrDefaultAsync(x => x.Blog_Id == id);
+
+            if (item == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+
+
+      
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, BlogDataModel requestModel)
+        {
+            var item = await _appdbcontext.Blogs.FirstOrDefaultAsync(x => x.Blog_Id == id);
+
+            if (item == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            item.Blog_Title = requestModel.Blog_Title;
+            item.Blog_Author = requestModel.Blog_Author;
+            item.Blog_Content = requestModel.Blog_Content;
+
+            await _appdbcontext.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        // ... (existing code)
+
     }
 }
